923370b2509bd0dc7dde9274007ebfc1cb6418d0
Santoshalimkar <santosh.sa4d@gmail.com>
2024-10-21 19:12:26 +0530

    Changes added

--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,19 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/react";
+import Loading from "./loading";
+import { useState } from "react";
 
 export function NextuiProviderWrapper({ children }) {
+const [loading, Setloading] = useState(true)
+
+setTimeout(() => {
+  Setloading(false)
+}, 5000);
+
+
+if(loading){
+  return <Loading/>
+}
+
   return <NextUIProvider>{children}</NextUIProvider>;
 }
