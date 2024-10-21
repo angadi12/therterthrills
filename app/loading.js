b1f90e9fec90d003ba1c94e5eec0eb3f0e5fdeed
Santoshalimkar <santosh.sa4d@gmail.com>
2024-10-21 19:21:37 +0530

    Changes added

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,12 @@
 "use client"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as animationData from "@/lib/Theaterthrills.json";
 import Lottie from 'react-lottie';
+import { motion, AnimatePresence } from "framer-motion";
 
 const Loading = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -13,9 +16,41 @@ const Loading = () => {
     },
   };
 
+  const title = [
+    "Birthday Party!",
+    "Anniversary!",
+    "Reunion!",
+    "Farewell!",
+    "Business Party!",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % title.length);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className=" bg-white absolute z-50 left-0 right-0 top-0 bottom-0 w-full h-screen flex justify-center items-center">
+    <div className=" bg-white absolute z-50 left-0 right-0 top-0 bottom-0 w-full h-screen flex flex-col gap-4 justify-center items-center">
       <Lottie options={defaultOptions} height={200} width={200} />
+      <AnimatePresence>
+            {title.map(
+              (value, index) =>
+                index === currentIndex && (
+                  <motion.p
+                    key={index}
+                    initial={{ opacity: 0, y: 15 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                    className="bg-clip-text text-3xl font-bold inline-block text-transparent bg-gradient-to-r from-[#F30278] via-[#F30278] to-[#E2B600]"
+                  >
+                    {value}.
+                  </motion.p>
+                )
+            )}
+          </AnimatePresence>
     </div>
   );
 };
