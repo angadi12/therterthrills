9d2c1289e9dad84f59ec1da6142be2a08188ec61
Santoshalimkar <santosh.sa4d@gmail.com>
2024-12-03 13:10:51 +0530

    Chnages added

--- app/bookings/page.js
+++ app/bookings/page.js
@@ -11,8 +11,8 @@ import {
   selectBookings,
 } from "@/lib/Redux/bookingSlice";
 import TheatreCardSkeleton from "@/components/Bookingcomponents/TheatreCardSkeleton";
-import {Tabs, Tab, Chip} from "@nextui-org/react";
-import { MonitorPlay,CalendarOff,ShoppingBag } from 'lucide-react';
+import { Tabs, Tab, Chip } from "@nextui-org/react";
+import { MonitorPlay, CalendarOff, ShoppingBag } from "lucide-react";
 
 const Page = () => {
   const dispatch = useDispatch();
@@ -20,7 +20,6 @@ const Page = () => {
   const bookings = useSelector(selectBookings);
   const loading = useSelector(selectBookingLoading);
   const error = useSelector(selectBookingError);
-
 
   const currentDate = new Date();
 
@@ -34,9 +33,6 @@ const Page = () => {
     const bookingDate = new Date(booking.date); // Adjust if the date field is named differently
     return bookingDate < currentDate;
   });
-
-
-
 
   useEffect(() => {
     dispatch(fetchBookingByUserId(user?._id));
@@ -55,82 +51,100 @@ const Page = () => {
         </p>
       </div>
 
-      <section className="w-11/12 mx-auto flex flex-col gap-4 justify-center items-start md:py-20 py-8">
+      <section className="w-11/12 mx-auto flex flex-col gap-4 justify-center items-center md:py-20 py-8">
         <p className="md:text-3xl text-xl font-medium md:mb-8">My Bookings</p>
-        <Tabs 
-        className="w-full"
-        aria-label="Options" 
-        color="primary" 
-        variant="underlined"
-        classNames={{
-            tabList:"gap-6 w-full relative   rounded-none p-2 border-b  border-divider",
+        <Tabs
+          className="w-full"
+          aria-label="Options"
+          color="primary"
+          variant="underlined"
+          classNames={{
+            tabList:
+              "gap-6 w-full relative   rounded-none p-2 border-b  border-divider",
             cursor: "w-full bg-[#F30278] ",
             tab: "w-full px-0 h-12 flex flex-col justify-center items-center",
             tabContent: "group-data-[selected=true]:text-[#004AAD] ",
           }}
-      >
-        <Tab
-          key="Active"
-          title={
-            <div className="flex items-center space-x-2">
-             <MonitorPlay className="text-tiny md:text-sm hidden md:block "/>
-              <span>Active Booking</span>
-              <Chip size="sm" className=" hidden md:flex justify-center items-center" color="danger" variant={`${activeBookings?.length===0?"faded":"solid"}`}>{activeBookings?.length}</Chip>
-            </div>
-          }
         >
-            <div className="w-full py-6 grid md:grid-cols-3 grid-cols-1 justify-center items-stretch place-content-center gap-4">
-          {loading ? (
-            Array.from({ length: 6 }).map((_, index) => (
-              <TheatreCardSkeleton key={index} />
-            ))
-          ) : (
-            <>
-              {activeBookings?.length === 0 ? (
-                <p className="col-span-3 text-center h-60 flex justify-center items-center w-full text-lg font-medium">
-                  No Booking available.
-                </p>
-              ) : (
-                activeBookings?.map((booking, index) => (
-                  <Bookingcard key={index} booking={booking} />
+          <Tab
+            key="Active"
+            title={
+              <div className="flex items-center space-x-2">
+                <MonitorPlay className="text-tiny md:text-sm hidden md:block " />
+                <span>Active Booking</span>
+                <Chip
+                  size="sm"
+                  className=" hidden md:flex justify-center items-center"
+                  color="danger"
+                  variant={`${
+                    activeBookings?.length === 0 ? "faded" : "solid"
+                  }`}
+                >
+                  {activeBookings?.length}
+                </Chip>
+              </div>
+            }
+          >
+            <div className="w-full mx-auto   py-6 grid md:grid-cols-3 grid-cols-1 justify-center items-stretch place-content-center gap-4 md:gap-8">
+              {loading ? (
+                Array.from({ length: 6 }).map((_, index) => (
+                  <TheatreCardSkeleton key={index} />
                 ))
+              ) : (
+                <>
+                  {activeBookings?.length === 0 ? (
+                    <p className="col-span-3  mx-auto text-center h-60 flex justify-center items-center w-full text-lg font-medium">
+                      No Booking available.
+                    </p>
+                  ) : (
+                    activeBookings?.map((booking, index) => (
+                      <Bookingcard key={index} booking={booking} />
+                    ))
+                  )}
+                </>
               )}
-            </>
-          )}
-        </div>
-        </Tab>
-        <Tab
-          key="Recent"
-          title={
-            <div className="flex items-center space-x-2">
-             <CalendarOff className=" hidden md:block"/>
-              <span>Recent Booking</span>
-              <Chip size="sm"  className=" hidden md:flex justify-center items-center"  color="danger" variant={`${recentBookings?.length===0?"faded":"solid"}`} >{recentBookings?.length}</Chip>
             </div>
-          }
-        >
-
-<div className="w-full py-6 grid md:grid-cols-3 grid-cols-1 justify-center items-stretch place-content-center gap-4">
-          {loading ? (
-            Array.from({ length: 6 }).map((_, index) => (
-              <TheatreCardSkeleton key={index} />
-            ))
-          ) : (
-            <>
-              {recentBookings?.length === 0 ? (
-                <p className="col-span-3 text-center h-60 flex justify-center items-center w-full text-lg font-medium">
-                  No Booking available.
-                </p>
-              ) : (
-                recentBookings?.map((booking, index) => (
-                  <Bookingcard key={index} booking={booking} />
+          </Tab>
+          <Tab
+            key="Recent"
+            title={
+              <div className="flex items-center space-x-2">
+                <CalendarOff className=" hidden md:block" />
+                <span>Recent Booking</span>
+                <Chip
+                  size="sm"
+                  className=" hidden md:flex justify-center items-center"
+                  color="danger"
+                  variant={`${
+                    recentBookings?.length === 0 ? "faded" : "solid"
+                  }`}
+                >
+                  {recentBookings?.length}
+                </Chip>
+              </div>
+            }
+          >
+            <div className="w-full py-6 mx-auto grid md:grid-cols-3 grid-cols-1 justify-center items-stretch place-content-center gap-4 md:gap-8">
+              {loading ? (
+                Array.from({ length: 6 }).map((_, index) => (
+                  <TheatreCardSkeleton key={index} />
                 ))
+              ) : (
+                <>
+                  {recentBookings?.length === 0 ? (
+                    <p className="col-span-3 mx-auto text-center h-60 flex justify-center items-center w-full text-lg font-medium">
+                      No Booking available.
+                    </p>
+                  ) : (
+                    recentBookings?.map((booking, index) => (
+                      <Bookingcard key={index} booking={booking} />
+                    ))
+                  )}
+                </>
               )}
-            </>
-          )}
-        </div>
-        </Tab>
-        {/* <Tab
+            </div>
+          </Tab>
+          {/* <Tab
           key="Cart"
           title={
             <div className="flex items-center space-x-2">
@@ -142,11 +156,7 @@ const Page = () => {
         >
 
         </Tab> */}
-      </Tabs>
-
-
-
-  
+        </Tabs>
       </section>
     </main>
   );
--- components/Bookingcomponents/Theatercard.js
+++ components/Bookingcomponents/Theatercard.js
@@ -175,7 +175,7 @@ export default function TheatreCard({ theatre }) {
           </div>
           <div className="flex items-center justify-between mb-2">
           <h3 className="text-xl font-bold mb-2">{theatre?.name}</h3>
-          {theatre?.availableSlots.length > 0 && <p className="text-white text-sm px-2 bg-[#F30278] rounded-full p-1">{`${theatre?.availableSlots.length} slots available`}</p>}
+          {theatre?.availableSlots.length > 0 && <p className="text-white md:text-sm text-xs px-2 bg-[#F30278] rounded-full p-1">{`${theatre?.availableSlots.length} slots available`}</p>}
           </div>
           <div className="flex gap-2 mb-2">
             <span className="bg-pink-100 ring-1 ring-[#F30278] text-xs text-[#F30278] px-2 py-2 font-semibold rounded flex items-center">
