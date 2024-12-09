fb882dd25a2bdc15c64ec95c5790d1f68feb73b9
Santoshalimkar <santosh.sa4d@gmail.com>
2024-12-09 17:32:32 +0530

    Changes added

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -6,7 +6,7 @@ import Recentblogs from '@/components/Blogscomponents/Recentblogs'
 const page = () => {
   return (
     <main className='pb-12 md:pb-10'>
-    <div className='relative justify-center items-center w-full '>
+    <div className='relative justify-center items-center w-full'>
     <Image src={mainblogs} alt='about us' className='relative brightness-50'/>
      <p className='absolute  md:text-3xl text-xl font-bold transform -translate-x-1/2 -translate-y-1/2 left-1/2 text-[#FFCE00] top-1/2'>Our Daily Blogs</p>
     </div>
